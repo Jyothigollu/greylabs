3949b756fc9581217bbd90c90ee5ac31203a09f4
Jyothigollu <gollujyothi123@gmail.com>
2024-08-22 23:16:13 +0530

    Add Procfile and update requirements

--- GreyLabsAPI/settings.py
+++ GreyLabsAPI/settings.py
@@ -152,6 +152,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 # Add this line if not already present
 
 #DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
